ce2174144ef912f448f4ac63c3aa398bf8fc104c
twd06 <twd06@LAPTOP-GC82N5EI>
2024-03-08 23:26:59 +0900

    03/08_준우_채팅

--- teamProject/src/main/java/com/team/chat/WebSocketHandler.java
+++ teamProject/src/main/java/com/team/chat/WebSocketHandler.java
@@ -11,6 +11,14 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+/*
+ * WebSocket Handler 작성
+ * 소켓 통신은 서버와 클라이언트가 1:n으로 관계를 맺는다. 따라서 한 서버에 여러 클라이언트 접속 가능
+ * 서버에는 여러 클라이언트가 발송한 메세지를 받아 처리해줄 핸들러가 필요
+ * TextWebSocketHandler를 상속받아 핸들러 작성
+ * 클라이언트로 받은 메세지를 log로 출력하고 클라이언트 환영 메세지를 보여줌
+ * */
+
 @Controller
 public class WebSocketHandler extends TextWebSocketHandler {
 	

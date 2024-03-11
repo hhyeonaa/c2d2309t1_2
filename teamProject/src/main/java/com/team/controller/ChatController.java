2ac52132255fe9c534a209e328df02c033962f62
y-e-o-p <duqdlduq2@naver.com>
2024-03-11 11:45:30 +0900

    1

--- teamProject/src/main/java/com/team/controller/ChatController.java
+++ teamProject/src/main/java/com/team/controller/ChatController.java
@@ -1,8 +1,10 @@
 package com.team.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.team.util.EnumCodeType;
@@ -16,8 +18,8 @@ public class ChatController {
 	private TeamCodeService codeService;
 	
 	@GetMapping("/chatting")
-	public String chatting() {
-		System.out.println("ChatController chatting()");
+	public String chatting(Model model, HttpSession session) {
+		model.addAttribute("dcm", codeService.selectCodeList(EnumCodeType.신고항목, session));
 		return "/chat/chatting";
 	}// chatting()
 	
